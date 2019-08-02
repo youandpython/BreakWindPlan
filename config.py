00a06286def0051b8a79851c1cb1759979e38101
GongFanghui <gongfanghui@aecg.com.cn>
2019-08-03 01:51:23 +0800

    加入定时任务,每天定时提醒天气

--- config.py
+++ config.py
@@ -8,3 +8,38 @@ import os
 cur_path = os.path.dirname(os.path.realpath(__file__))
 # 获取QR.png的路径
 qr_path = os.path.join(cur_path, 'QR.png')
+
+# 机器人主人, 使用备注名更安全，只允许一个，可远程控制机器人，如果不设置(空)则将文件助手设置为管理员，但不具备远程控制功能
+bot_master_name = 'DoggerX'
+
+# 好友自动回复
+is_friend_auto_reply = True
+# 此项表示群中是否回复
+is_group_reply = True
+# True:@本人才回復，上一项开启后此项才生效
+is_group_at_reply = False
+# 开启防撤回模式
+is_forward_revoke_msg = True
+
+# 监听某些好友群聊，如老板
+is_listen_friend = True
+# 在这些群里监听好友说的话，匹配模式：包含“唯一集团工作群”的群
+listen_friend_groups = '游侠客全球领队颜值担当|擦鞋匠交流群|万里阳光-户外休闲社|冲鸭!每天学点Python-56'
+# 需要监听的人名称，使用备注名更安全，允许多个用|分隔，如：主管|项目经理|产品狗
+listen_friend_names = ''
+
+# 打开转发模式，主人发送给机器人的消息都将转发至forward_groups群
+is_forward_mode = True
+# 需要将消息转发的群，匹配模式同上
+forward_groups = '游侠客全球领队颜值担当'
+
+# 群分享监控
+is_listen_sharing = True
+# 监控群分享，匹配模式同上
+listen_sharing_groups = '游侠客全球领队颜值担当'
+
+# 定时提醒：每天几点几分
+timing_hour = 7
+timing_minute = 30
+timing_remind_groups = '游侠客全球领队颜值担当'
+timing_location = '南京'
---
+++ utils/ap_scheduler.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def _test():
+    print('Test !!!')
+
+
+def timely_execute(function, hour, minute, args):
+    """定时每天hour点minute分执行方法"""
+
+    # BackgroundScheduler: 适合于要求任何在程序后台运行的情况，当希望调度器在应用后台执行时使用。
+    scheduler = BackgroundScheduler()
+    # 采用非阻塞的方式，间隔3秒钟执行一次
+    scheduler.add_job(function, 'cron', hour=hour, minute=minute, args=args)
+    # 这是一个独立的线程
+    scheduler.start()
+
+
+if __name__ == '__main__':
+    timely_execute(_test, 7, 30, [])
+    # while True:
+    #     print('main-start:', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    #     time.sleep(3)
+    #     print('main-end:', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
