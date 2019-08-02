8d6edcda34bbd029f36dc82886d5b60aa94eaa63
GongFanghui <gongfanghui@aecg.com.cn>
2019-08-03 01:50:29 +0800

    完善天气查询,支持国内外城市天气查询

--- utils/weather.py
+++ utils/weather.py
@@ -5,29 +5,80 @@ import json
 import re
 
 import requests
+import xml.dom.minidom as xmldom
+
+# def get(city_name):
+#     url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + city_name
+#     response = requests.get(url)
+#     weather_json = json.loads(response.text)
+#     info = list()
+#     if "data" not in weather_json:
+#         info.append('未查询到{}'.format(city_name))
+#     else:
+#         a = weather_json['data']
+#         info.append('{0} 当前温度：{1}℃'.format(a['city'], a['wendu']))
+#         info.append('温馨提示：{}'.format(a['ganmao']))
+#         info.append("-----------------------------")
+#         for i in range(1, 3):
+#             info.append('{0} {1}'.format(a["forecast"][i]['date'], a["forecast"][i]['type']))
+#             feng_li = a["forecast"][i]['fengli'][9: [m.start() for m in re.finditer(']', a['yesterday']['fl'])][0]]
+#             info.append('{0} {1}'.format(a["forecast"][i]['fengxiang'], feng_li))
+#             info.append(a["forecast"][i]['high'])
+#             info.append(a["forecast"][i]['low'])
+#             info.append("-----------------------------")
+#     return '\r\n'.join(info)
 
 
 def get(city_name):
-    url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + city_name
+    url = 'http://ws.webxml.com.cn//WebServices/WeatherWS.asmx/getWeather?theCityCode={}&theUserID='.format(city_name)
     response = requests.get(url)
-    weather_json = json.loads(response.text)
+    dom_obj = xmldom.parseString(response.text)
+    # 得到元素对象
+    obj_list = dom_obj.documentElement.getElementsByTagName("string")
+    info_list = [item.firstChild.data for item in obj_list]
     info = list()
-    if "data" not in weather_json:
-        info.append('未查询到{}'.format(city_name))
+    if len(info_list) == 1:
+        info.append('暂查询不到{}'.format(city_name))
     else:
-        a = weather_json['data']
-        info.append('{0} 当前温度：{1}℃'.format(a['city'], a['wendu']))
-        info.append('温馨提示：{}'.format(a['ganmao']))
+        # 0: 省份 地区/洲 国家名（国外）
+        # 1: 查询的天气预报地区名称
+        city = info_list[0] if info_list[1] in info_list[0] else '{} {}'.format(info_list[0], info_list[1])
+        info.append('{}：'.format(city))
+        # 4: 当前实况：气温、风向/风力、湿度
+        live_situations = info_list[4][7:].split('；')
+        if len(live_situations) == 3:
+            info.append('当前{}'.format(live_situations[0]))
+            info.append(live_situations[1])
+            info.append(live_situations[2])
+        # # 5: 当前空气质量、紫外线强度
+        # info.append(info_list[5])
+        # # 6: 当前天气和生活指数
+        # info.append(info_list[6].rstrip('\n'))
         info.append("-----------------------------")
-        for i in range(1, 3):
-            info.append('{0} {1}'.format(a["forecast"][i]['date'], a["forecast"][i]['type']))
-            feng_li = a["forecast"][i]['fengli'][9: [m.start() for m in re.finditer(']', a['yesterday']['fl'])][0]]
-            info.append('{0} {1}'.format(a["forecast"][i]['fengxiang'], feng_li))
-            info.append(a["forecast"][i]['high'])
-            info.append(a["forecast"][i]['low'])
-            info.append("-----------------------------")
+        # 7: 当前天气 概况 格式：M月d日 天气概况
+        info.append(info_list[7])
+        # 8: 当前气温
+        info.append('最低/高温：{}'.format(info_list[8]))
+        # 9: 当前 风力/风向
+        info.append('风力/风向：{}'.format(info_list[9]))
+        info.append("-----------------------------")
+        # 12: 明天天气 概况 格式：M月d日 天气概况
+        info.append(info_list[12])
+        # 13: 明天气温
+        info.append('最低/高温：{}'.format(info_list[13]))
+        # 14: 明天 风力/风向
+        info.append('风力/风向：{}'.format(info_list[14]))
+        info.append("-----------------------------")
+        # 17: 后天天气 概况 格式：M月d日 天气概况
+        info.append(info_list[17])
+        # 18: 后天气温
+        info.append('最低/高温：{}'.format(info_list[18]))
+        # 19: 后天 风力/风向
+        info.append('风力/风向：{}'.format(info_list[19]))
+        info.append("-----------------------------")
     return '\r\n'.join(info)
 
 
 if __name__ == '__main__':
-    get('南京')
+    result = get('伦敦')
+    # print(result)
