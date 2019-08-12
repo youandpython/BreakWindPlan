e063b0a8efeba02f2a906b5cd590c102ac2881dd
GongFanghui <gongfanghui@aecg.com.cn>
2019-08-12 11:20:18 +0800

    定时天气提醒优化，本地使用详细信息

--- utils/weather.py
+++ utils/weather.py
@@ -5,6 +5,7 @@ import json
 import re
 
 import requests
+import xml.dom.minidom as xmldom
 
 
 def get(city_name):
@@ -34,56 +35,59 @@ def translate(city_name):
     regex = re.compile(pattern)
     results = regex.findall(city_name)
     return ''.join(results)
-# def get(city_name):
-#     url = 'http://ws.webxml.com.cn//WebServices/WeatherWS.asmx/getWeather?theCityCode={}&theUserID='.format(city_name)
-#     response = requests.get(url)
-#     dom_obj = xmldom.parseString(response.text)
-#     # 得到元素对象
-#     obj_list = dom_obj.documentElement.getElementsByTagName("string")
-#     info_list = [item.firstChild.data for item in obj_list]
-#     info = list()
-#     if len(info_list) == 1:
-#         info.append('暂查询不到{}'.format(city_name))
-#     else:
-#         # 0: 省份 地区/洲 国家名（国外）
-#         # 1: 查询的天气预报地区名称
-#         city = info_list[0] if info_list[1] in info_list[0] else '{} {}'.format(info_list[0], info_list[1])
-#         info.append('{}：'.format(city))
-#         # 4: 当前实况：气温、风向/风力、湿度
-#         live_situations = info_list[4][7:].split('；')
-#         if len(live_situations) == 3:
-#             info.append('当前{}'.format(live_situations[0]))
-#             info.append(live_situations[1])
-#             info.append(live_situations[2])
-#         # # 5: 当前空气质量、紫外线强度
-#         # info.append(info_list[5])
-#         # # 6: 当前天气和生活指数
-#         # info.append(info_list[6].rstrip('\n'))
-#         info.append("-----------------------------")
-#         # 7: 当前天气 概况 格式：M月d日 天气概况
-#         info.append(info_list[7])
-#         # 8: 当前气温
-#         info.append('最低/高温：{}'.format(info_list[8]))
-#         # 9: 当前 风力/风向
-#         info.append('风力/风向：{}'.format(info_list[9]))
-#         info.append("-----------------------------")
-#         # 12: 明天天气 概况 格式：M月d日 天气概况
-#         info.append(info_list[12])
-#         # 13: 明天气温
-#         info.append('最低/高温：{}'.format(info_list[13]))
-#         # 14: 明天 风力/风向
-#         info.append('风力/风向：{}'.format(info_list[14]))
-#         info.append("-----------------------------")
-#         # 17: 后天天气 概况 格式：M月d日 天气概况
-#         info.append(info_list[17])
-#         # 18: 后天气温
-#         info.append('最低/高温：{}'.format(info_list[18]))
-#         # 19: 后天 风力/风向
-#         info.append('风力/风向：{}'.format(info_list[19]))
-#         info.append("-----------------------------")
-#     return '\r\n'.join(info)
+
+
+def get_timing(city_name):
+    city_name = translate(city_name)
+    url = 'http://ws.webxml.com.cn//WebServices/WeatherWS.asmx/getWeather?theCityCode={}&theUserID='.format(city_name)
+    response = requests.get(url)
+    dom_obj = xmldom.parseString(response.text)
+    # 得到元素对象
+    obj_list = dom_obj.documentElement.getElementsByTagName("string")
+    info_list = [item.firstChild.data for item in obj_list]
+    info = list()
+    if len(info_list) == 1:
+        info.append('暂查询不到{}'.format(city_name))
+    else:
+        # 0: 省份 地区/洲 国家名（国外）
+        # 1: 查询的天气预报地区名称
+        city = info_list[0] if info_list[1] in info_list[0] else '{} {}'.format(info_list[0], info_list[1])
+        info.append('{}：'.format(city))
+        # 4: 当前实况：气温、风向/风力、湿度
+        live_situations = info_list[4][7:].split('；')
+        if len(live_situations) == 3:
+            info.append('当前{}'.format(live_situations[0]))
+            info.append(live_situations[1])
+            info.append(live_situations[2])
+        # 5: 当前空气质量、紫外线强度
+        info.append(info_list[5])
+        # 6: 当前天气和生活指数
+        info.append(info_list[6].rstrip('\n'))
+        info.append("-----------------------------")
+        # 7: 当前天气 概况 格式：M月d日 天气概况
+        info.append(info_list[7])
+        # 8: 当前气温
+        info.append('最低/高温：{}'.format(info_list[8]))
+        # 9: 当前 风力/风向
+        info.append('风力/风向：{}'.format(info_list[9]))
+        info.append("-----------------------------")
+        # 12: 明天天气 概况 格式：M月d日 天气概况
+        info.append(info_list[12])
+        # 13: 明天气温
+        info.append('最低/高温：{}'.format(info_list[13]))
+        # 14: 明天 风力/风向
+        info.append('风力/风向：{}'.format(info_list[14]))
+        info.append("-----------------------------")
+        # 17: 后天天气 概况 格式：M月d日 天气概况
+        info.append(info_list[17])
+        # 18: 后天气温
+        info.append('最低/高温：{}'.format(info_list[18]))
+        # 19: 后天 风力/风向
+        info.append('风力/风向：{}'.format(info_list[19]))
+        info.append("-----------------------------")
+    return '\r\n'.join(info)
 
 
 if __name__ == '__main__':
-    result = get('南 京')
+    result = get_timing('南 京')
     print(result)
