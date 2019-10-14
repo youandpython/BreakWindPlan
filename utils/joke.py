0edc0584dc2b3ee4a14396b495551c765a2502fa
Dogger <1154306644@qq.com>
2019-10-14 11:01:28 +0800

    笑话完善

--- utils/joke.py
+++ utils/joke.py
@@ -12,6 +12,16 @@ rule_url = re.compile('<a href=\"(.*?)\"target=\"_blank\" >')
 mainUrl = 'http://www.jokeji.cn'
 url = 'http://www.jokeji.cn/list.htm'
 
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; ) AppleWebKit/534.12 (KHTML, like Gecko) Maxthon/3.0 Safari/534.12',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E) QQBrowser/6.9.11079.201',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)'
+    ]
+
 
 def get_random_param(count: int):
     return ''.join(random.sample(string.ascii_letters, count))
@@ -19,7 +29,8 @@ def get_random_param(count: int):
 
 def get():
     try:
-        resp = requests.get(f'{url}?{get_random_param(6)}={get_random_param(4)}', headers=SPIDER_HEADERS)
+        user_agent = {'User-Agent': random.sample(user_agent_list, 1)[0]}
+        resp = requests.get(url, headers=user_agent)
         if resp.status_code == 200:
             urls = rule_url.findall(resp.content.decode('GBK'))
             if len(urls) <= 0:
@@ -27,7 +38,7 @@ def get():
             index = random.sample(range(len(urls)), 1)[0]
 
             joke_url = mainUrl + urls[index]
-            resp2 = requests.get(joke_url, headers=SPIDER_HEADERS)
+            resp2 = requests.get(joke_url, headers=user_agent)
             if resp2.status_code == 200:
                 joke_str = rule_joke.findall(resp2.content.decode('GBK'))
                 jokes = joke_str[0].split('<P>')[1:]
